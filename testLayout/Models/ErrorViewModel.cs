69cf356c64080893b840d7174cf532edd6f4b39c
nmdbn <lam2012004@gmail.com>
2024-11-26 22:27:25 +0700

    Add project files.

---
+++ testLayout/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testLayout.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
